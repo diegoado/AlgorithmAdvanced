604928fcbd5c92b94d86cd63daedfccb00c1fee1
Diego Adolfo <diegoado@gmail.com>
2016-09-03 12:08:04 -0300

    Problem ACM-ICPC 3571: Visible Lattice Points accept

---
+++ Intermediary/number-theory/VisibleLatticePoints.cpp
@@ -0,0 +1,46 @@
+/*
+ * University Federal of Campina Grande
+ * Student: Diego Adolfo Silva de Araújo
+ * Registry: 113210090
+ * Discipline: Algoritmos Avançados
+ *
+ * ACM-ICPC Live Archive
+ * Problem: 3571 - Visible Lattice Points
+ * Time limit per test: 3 seconds
+ * Memory limit per test: 256 megabytes
+ * Input: standard input
+ * Output: standard output
+ */
+
+#include <iostream>
+
+#define MAX_N 1001
+
+using namespace std;
+
+int grip[MAX_N];
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int main() {
+    int t, n;
+
+    grip[0] = 1;
+    for (int i = 1; i < MAX_N; i++) {
+        grip[i] = grip[i-1];
+
+        for (int j = 1; j <= i; j++) {
+            if (gcd(j, i) == 1)
+                grip[i] += 2;
+        }
+    }
+    scanf("%d", &t);
+    for (int k = 1; k <= t; k++) {
+        scanf("%d", &n);
+        printf("%d %d %d\n", k, n, grip[n]);
+    }
+
+    return 0;
+}
